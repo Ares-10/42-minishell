8ec79bd735dc43958e87083f5841b0696148c7bd
Ares-10 <cholionking1@naver.com>
2024-06-30 10:06:08 +0900

    refactor: parse 로직에서 tokenize 로직 분리

--- parse/parse.h
+++ parse/parse.h
@@ -6,7 +6,7 @@
 /*   By: hyungcho <hyungcho@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 04:23:19 by hyungcho          #+#    #+#             */
-/*   Updated: 2024/06/30 04:23:29 by hyungcho         ###   ########.fr       */
+/*   Updated: 2024/06/30 09:06:55 by hyungcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 
 # include "../minishell.h"
 
-int		get_token_size(const char *str);
-t_token	*new_token(const char *str, int size);
-void	delete_token(t_token *token);
+# define WORD 0
+# define PIPE 1
+# define REDIRECT 2
 
+void	tokenize(t_list **token_list, char *str);
+
+void	delete_token(void *token);
 int		check_quote(const char *str, int index);
 
 #endif
