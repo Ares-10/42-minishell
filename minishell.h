bc4017b87cb17d98b3361ef784b0a1f28100d57b
dlstodfpwjsem <seojepar@student.42seoul.kr>
2024-06-30 21:05:19 +0900

    Docs: minishell.h 구조체 변수 추가

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: hyungcho <hyungcho@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 17:02:44 by hyungcho          #+#    #+#             */
-/*   Updated: 2024/06/24 17:28:56 by hyungcho         ###   ########.fr       */
+/*   Updated: 2024/06/29 01:09:24 by seojepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,29 @@
 
 # include "Libft/libft.h"
 # include <readline/readline.h>
+
+/*
+ * token
+ */
+
+# define T_NULL 0
+# define T_WORD 1
+# define T_PIPE 2
+# define T_REDIRECT 3
+# define T_DOUBLE_QUOTES 4
+# define T_SINGLE_QUOTES 5
+
+typedef struct s_token
+{
+	int		type;
+	char	*str;
+}	t_token;
+
+typedef struct s_ast
+{
+	t_token	token;
+	t_ast	*left;
+	t_ast	*right;
+}	t_ast;
 
 #endif
