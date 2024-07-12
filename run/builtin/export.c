d9964d38ce819db2e96db7bac4563565d0182d29
dlstodfpwjsem <seojepar@student.42seoul.kr>
2024-07-12 21:09:23 +0900

    Chore: export.c 에러메세지 printf가 아니라 STDERR로 수정

--- run/builtin/export.c
+++ run/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: seojepar <seojepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 21:41:30 by seojepar          #+#    #+#             */
-/*   Updated: 2024/07/12 13:14:22 by seojepar         ###   ########.fr       */
+/*   Updated: 2024/07/12 21:00:16 by seojepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,13 @@
 
 static void	change_env(char ***env, char *arg, int idx)
 {
-	// 원래 환경변수가 동적할당 된거 같지 않은데..
-	// 환경변수를 그대로 복제해서 패스해준다고 가정해야겠다..
-	// 그럼 free가 가능해지니까
 	free((*env)[idx]);
 	(*env)[idx] = ft_strdup(arg);
 	if ((*env)[idx] == NULL)
 		puterr_exit("malloc failed");
 }
 
-static int	add_env(char ***env, char *var)
+int	ft_setenv(char ***env, char *var)
 {
 	int	i;
 
@@ -51,6 +48,19 @@ static int	find_key_in_env(char *key, char **env)
 	return (-1);
 }
 
+int	valid_shell_name(char *name)
+{
+	if (ft_isdigit(*name))
+		return (FALSE);
+	while (*name != '=')
+	{
+		if (!(ft_isalnum(*name) || *name == '_'))
+			return (FALSE);
+		name++;
+	}
+	return (TRUE);
+}
+
 void	builtin_export(char **argv, char ***env)
 {
 	int		i;
@@ -60,7 +70,7 @@ void	builtin_export(char **argv, char ***env)
 
 	if (argv[1] == NULL)
 	{
-		i = 0;
+		i = 1;
 		while((*env)[i] != NULL)
 		{
 			printf("declare -x %s\n", (*env)[i]);
@@ -77,26 +87,44 @@ void	builtin_export(char **argv, char ***env)
 		while(argv[i] != NULL)
 		{
 			// =가 없으면, 무시한다.
-			printf("1\n");
 			equal = ft_strchr(argv[i], '=');
 			if (equal == 0)
+			{
+				i++;
 				continue ;
+			}
 			// =로 시작한다면 invalid 취급한다.
 			if (equal == argv[i])
-				printf("minishell: export: `%s': not a valid identifier\n", argv[i]);				
+			{
+				write_error("minishell: export: ");
+				write_error(argv[i]);
+				write_error(": not a valid identifier\n");
+				i++;
+				continue ;
+			}
 			// key가 있는 확인하자. : 찝찝한것: getenv를 써야될거 같아서.. 
 			// 근데 그건, unistd.h 내부의 environ 전역변수에 접근해서 검색하는 로직이라, 
 			// 전역변수를 시그널 말고는 사용할 수 없는 현재 서브젝트에는 적용이 불가능하다.
 			key = ft_substr(argv[i], 0, equal - argv[i] + 1);
 			if (key == NULL)
 				puterr_exit("malloc failed");
+			if (valid_shell_name(key) == FALSE)
+			{
+				write_error("minishell: export: ");
+				write_error(argv[i]);
+				write_error(": not a valid identifier\n");
+				i++;
+				continue ;
+			}
 			// key가 원래 환경변수목록에 있는지 확인한다. 있으면 index를 반환, 없으면 음수
 			idx = find_key_in_env(key, *env);
 			if (idx < 0)
-				add_env(env, argv[i]);
+				ft_setenv(env, argv[i]);
 			else
 				change_env(env, argv[i], idx);
 			i++;
 		}
+		if (i != 1)
+			free(key);
 	}
 }
