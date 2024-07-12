4f5f6af4e98ec1043bcacb6abd0c1ab0299465be
dlstodfpwjsem <seojepar@student.42seoul.kr>
2024-07-12 21:11:30 +0900

    Chore: run.c의 init_pipe에서 자녀프로세스 개수 0으로 초기화 추가

--- run/run.c
+++ run/run.c
@@ -6,7 +6,7 @@
 /*   By: seojepar <seojepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 11:43:58 by seojepar          #+#    #+#             */
-/*   Updated: 2024/07/12 12:25:38 by seojepar         ###   ########.fr       */
+/*   Updated: 2024/07/12 21:07:34 by seojepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	handle_redirect(t_tree *node, char **env, t_pipe *info);
 void	init_pipe(t_pipe **info)
 {
 	*info = xmalloc(sizeof(t_pipe));
+	(*info)->total_child_cnt = 0;
 	if (pipe((*info)->prev_fd) < 0)
 	{
 		perror("pipe");
@@ -49,16 +50,6 @@ void	exec_tree(t_tree *node, char **env, t_pipe *info)
 
 void	handle_pipe(t_tree *node, char **env, t_pipe *info)
 {
-	// 파이프를 만나면 해야할 일: 
-	// 뒤에 파이프가 없다면 1로 리다이렉팅,
-	// 뒤에 파이프가 있다면 파이프의 write로 리다이렉팅.
-	// 앞에 파이프가 있었다면 그건 어차피 항상 0으로 왔을거라 알바 아님.
-	// N개의 파이프가 새로 파이프를 만들기보다 파이프끼리 파이프를 공유해서
-	// 사용할 수 있어야 함. 공유 파이프를 어떻게 전달할 것인가?
-	// 포크를 떠서 프로세스 종료시까지 기다려야한다.
-	// 그건 여기서 하지 말고, cmd 노드에서 포크 뜨고 실행시키는걸로 하자!
-	// 왜냐면 포크를 뜨는 순간 프로세스가 복제가 되서 노드를 중복 순회할 수도.
-
 	if (node->right)
 		info->next_pipe_exist = TRUE;
 	else
@@ -70,7 +61,15 @@ void	handle_here_document(t_redirect *redirect, char **env)
 	char	*line;
 	int		tmp_fd;
 	char	*tmp_filename;
+	struct termios	term;
 
+	// 에코 끔
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_lflag &= ~(ECHOCTL);
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+	// 무시
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
 	line = NULL;
 	// tmp_filename = tmpnam(NULL);
 	tmp_filename = ".tmp";
@@ -84,10 +83,8 @@ void	handle_here_document(t_redirect *redirect, char **env)
 	{
 		line = readline("> ");
 		if (!line)
-		{
 			break ;
-		}
-		if (strcmp(line, redirect->file_path) == 0)
+		if (ft_strcmp(line, redirect->file_path) == 0)
 		{
 			free(line);
 			break ;
@@ -106,6 +103,12 @@ void	handle_here_document(t_redirect *redirect, char **env)
 	dup2(tmp_fd, STDIN_FILENO);
 	close(tmp_fd);
 	unlink(tmp_filename);
+	// 시그널 복구
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	// 에코 킴
+	term.c_lflag |= ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
 }
 
 void	handle_redirect(t_tree *node, char **env, t_pipe *info)
@@ -135,6 +138,4 @@ void	handle_redirect(t_tree *node, char **env, t_pipe *info)
 	else if (redirect->type == INPUT_REDIRECT)
 		dup2(fd, STDIN_FILENO);
 	close(fd);
-	if (node->right)
-		exec_command(node->right, env, info);
 }
