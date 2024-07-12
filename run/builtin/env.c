d3d7b9c3e89e2c3dfff4cdc092f4f937c45ae734
dlstodfpwjsem <seojepar@student.42seoul.kr>
2024-07-12 21:09:59 +0900

    Fix: env.c env[0]의 ? 제외하고 출력 수정

--- run/builtin/env.c
+++ run/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: seojepar <seojepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 21:45:48 by seojepar          #+#    #+#             */
-/*   Updated: 2024/07/12 12:43:53 by seojepar         ###   ########.fr       */
+/*   Updated: 2024/07/12 15:12:49 by seojepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@ void	builtin_env(char **argv, char **env)
 {
 	int	i;
 
-	i = 0;
 	if (argv[1] != NULL)
 	{
 		printf("env: %s: No such file or directory", argv[1]);
 		return ;
 	}
+	// 0은 ? 있어서 제외
+	i = 1;
 	while (env[i] != NULL)
 	{
 		printf("%s\n", env[i]);
