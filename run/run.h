b3d003c07417f8828949df9484d451288af826e1
dlstodfpwjsem <seojepar@student.42seoul.kr>
2024-07-12 13:24:13 +0900

    Chore: run.h에 각종 함수 인자 수정 반영

--- run/run.h
+++ run/run.h
@@ -6,7 +6,7 @@
 /*   By: seojepar <seojepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 21:40:28 by seojepar          #+#    #+#             */
-/*   Updated: 2024/07/07 22:35:38 by seojepar         ###   ########.fr       */
+/*   Updated: 2024/07/12 12:42:18 by seojepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,20 @@
 # include <sys/wait.h>
 # define TRUE 1
 # define FALSE 0
+# define R 0
+# define W 1
 
 int		ft_strcmp(const char *s1, const char *s2);
 
-void	exec_argv(char *cmd, char **env);
+void	exec_argv(char *cmd, char **argv, char **env);
+void	exec_command(t_tree *node, char **env, t_pipe *info);
 
 void	builtin_echo(char **argv);
 void	builtin_cd(char **argv, char **env);
 void	builtin_pwd(void);
 void	builtin_export(char **argv, char ***env);
 void	builtin_unset(char **argv, char ***env);
-void	builtin_env(char **env);
+void	builtin_env(char **argv, char **env);
 void	builtin_exit(char **argv);
 
 #endif
