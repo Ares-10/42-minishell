bc05b16c496dc26c2af99e089c366ad7c849d39b
dlstodfpwjsem <seojepar@student.42seoul.kr>
2024-07-12 21:44:49 +0900

    Chore: error_and_exit 함수 추가

--- run/error.c
+++ run/error.c
@@ -6,7 +6,7 @@
 /*   By: seojepar <seojepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 20:53:31 by seojepar          #+#    #+#             */
-/*   Updated: 2024/07/12 20:54:10 by seojepar         ###   ########.fr       */
+/*   Updated: 2024/07/12 21:35:10 by seojepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,10 @@
 void	write_error(char *msg)
 {
 	write(STDERR_FILENO, msg, ft_strlen(msg));
+}
+
+void	error_and_exit(char *msg)
+{
+	write_error(msg);
+	exit(EXIT_FAILURE);
 }
