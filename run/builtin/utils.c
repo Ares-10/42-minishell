e3d0245278a19b2a668ab23db79e10bc172e5f5b
dlstodfpwjsem <seojepar@student.42seoul.kr>
2024-07-12 13:21:34 +0900

    Chore: 파이프 구조체의 prev_pipe변수 추가로 인해 각종 함수 이전/이후에 변수 접근 추가

--- run/builtin/utils.c
+++ run/builtin/utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtin_utils.c                                    :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seojepar <seojepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 21:33:06 by seojepar          #+#    #+#             */
-/*   Updated: 2024/07/07 22:33:53 by seojepar         ###   ########.fr       */
+/*   Updated: 2024/07/12 12:42:05 by seojepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,9 @@ int	ft_strcmp(const char *s1, const char *s2)
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
 
-int	execute_builtin(t_simplecmd *cmd, char **env)
+int	execute_builtin(t_simplecmd *cmd, char **env, t_pipe *info)
 {
+	info->prev_pipe_exist = TRUE;
 	if (ft_strcmp(cmd->file_path, "echo") == 0)
 		builtin_echo(cmd->argv);
 	else if (ft_strcmp(cmd->file_path, "cd") == 0)
@@ -35,19 +36,62 @@ int	execute_builtin(t_simplecmd *cmd, char **env)
 	else if (ft_strcmp(cmd->file_path, "unset") == 0)
 		builtin_unset(cmd->argv, &env);
 	else if (ft_strcmp(cmd->file_path, "env") == 0)
-		builtin_env(env);
+		builtin_env(cmd->argv, env);
 	else if (ft_strcmp(cmd->file_path, "exit") == 0)
 		builtin_exit(cmd->argv);
 	else
+	{
+		info->prev_pipe_exist = FALSE;
 		return (0);
+	}
 	return (1);
 }
 
-void	exec_command(t_tree *node, char **env)
+void	pexit(char *msg)
+{
+	printf("%s", msg);
+	exit(1);
+}
+
+void	exec_command(t_tree *node, char **env, t_pipe *info)
 {
 	t_simplecmd	*cmd;
+	int			new_fd[2];
+	pid_t		pid;
 
 	cmd = (t_simplecmd *)node->data;
-	if (execute_builtin(cmd, env) == FALSE)
-		exec_argv(cmd->file_path, env);
+	if (execute_builtin(cmd, env, info) == FALSE)
+	{
+		if (info->next_pipe_exist && pipe(new_fd) < 0)
+			pexit("Pipe Failed");
+		pid = fork();
+		if (pid == 0)
+		{
+			if (info->prev_pipe_exist)
+			{
+				dup2(info->prev_fd[R], STDIN_FILENO);
+				close(info->prev_fd[R]);
+				close(info->prev_fd[W]);
+			}
+			if (info->next_pipe_exist)
+			{
+				dup2(new_fd[W], STDOUT_FILENO);
+				close(new_fd[R]);
+				close(new_fd[W]);
+			}
+			exec_argv(cmd->file_path, cmd->argv, env);
+		}
+		else 
+		{
+			close(info->prev_fd[R]);
+			close(info->prev_fd[W]);
+			if (info->next_pipe_exist)
+			{
+				info->prev_fd[R] = new_fd[R];
+				info->prev_fd[W] = new_fd[W];
+			}
+			info->prev_pipe_exist = TRUE;
+			waitpid(pid, NULL, 0);
+		}
+	}
 }
