ff5d0ebe50d35cc6430c730597f76ddbf129b6e7
seojepar <146311443+seojepar@users.noreply.github.com>
2024-07-14 00:22:11 +0900

    Feat: echo -n 처리 추가

--- run/builtin/simple.c
+++ run/builtin/simple.c
@@ -6,23 +6,50 @@
 /*   By: seojepar <seojepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 21:38:24 by seojepar          #+#    #+#             */
-/*   Updated: 2024/07/12 20:57:47 by seojepar         ###   ########.fr       */
+/*   Updated: 2024/07/13 18:04:43 by seojepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "run.h"
+int	check_if_option(char *str)
+{
+	char	*tmp = str;
+	if (*str != '-' || *(str + 1) == '\0')
+		return (FALSE);
+	str++;
+	while (*str)
+	{
+		if (*str != 'n')
+			return (FALSE);
+		str++;
+	}
+	return (TRUE);
+}
 
 void	builtin_echo(char **argv)
 {
 	int	i;
+	int	need_newline;
 
 	i = 1;
-	if (argv[i])
+	need_newline = TRUE;
+	while (argv[i])
 	{
-		printf("%s\n", argv[1]);
+		if (check_if_option(argv[i]))
+			need_newline = FALSE;
+		else
+			break ;
+		i++;
+	}
+	while (argv[i])
+	{
+		printf("%s", argv[i]);
 		if (argv[i + 1])
 			printf(" ");
+		i++;
 	}
+	if (need_newline)
+		printf("\n");
 }
 
 void	builtin_cd(char **argv, char **env)
