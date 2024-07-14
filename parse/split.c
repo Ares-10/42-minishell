8b7b288bc33f7b8744ce585809e10ac10d28c580
Ares-10 <cholionking1@naver.com>
2024-07-14 15:36:51 +0900

    fix: 세미콜론(;) 처리

--- parse/split.c
+++ parse/split.c
@@ -6,13 +6,13 @@
 /*   By: hyungcho <hyungcho@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 18:51:07 by hyungcho          #+#    #+#             */
-/*   Updated: 2024/07/07 19:16:47 by hyungcho         ###   ########.fr       */
+/*   Updated: 2024/07/14 15:22:40 by hyungcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 
-static int	word_count(char const *s)
+static int	word_count(char const *s, char c)
 {
 	int	count;
 	int	i;
@@ -20,39 +20,39 @@ static int	word_count(char const *s)
 	count = 0;
 	i = -1;
 	while (s[++i])
-		if ((s[i] == '$' && check_quote(s, i) != 1) || i == 0)
+		if ((s[i] == c && check_quote(s, i) != 1) || i == 0)
 			count++;
-	if (s[0] == '$')
+	if (s[0] == c)
 		count++;
 	return (count);
 }
 
-static char	*get_word(char const *s, int i)
+static char	*get_word(char const *s, const int i, char c)
 {
 	int		len;
 
 	len = 1;
 	while (s[len + i]
-		&& !(s[len + i] == '$' && check_quote(s, len + i) != 1))
+		&& !(s[len + i] == c && check_quote(s, len + i) != 1))
 		len++;
 	return (ckm(ft_substr(s, i, len)));
 }
 
 /* safe */
-char	**var_split(char const *s)
+char	**nquote_split(char const *s, char c)
 {
 	char	**strs;
 	int		j;
 	int		i;
 
-	strs = (char **)xmalloc(sizeof(char *) * (word_count(s) + 1));
+	strs = (char **)xmalloc(sizeof(char *) * (word_count(s, c) + 1));
 	j = 0;
 	i = 0;
-	if (s[0] == '$')
+	if (s[0] == c)
 		strs[j] = ckm(ft_strdup(""));
 	while (s[i] != '\0')
 	{
-		strs[j] = get_word(s, i);
+		strs[j] = get_word(s, i, c);
 		i += ft_strlen(strs[j]);
 		j++;
 	}
