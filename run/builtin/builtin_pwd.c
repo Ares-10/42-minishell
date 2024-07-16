31b972c68429272e2444dd6862cad845f304826b
dlstodfpwjsem <seojepar@student.42seoul.kr>
2024-07-16 17:03:13 +0900

    feat: pwd 함수 exit status 처림

--- run/builtin/builtin_pwd.c
+++ run/builtin/builtin_pwd.c
@@ -6,18 +6,26 @@
 /*   By: seojepar <seojepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 16:46:50 by seojepar          #+#    #+#             */
-/*   Updated: 2024/07/16 16:47:00 by seojepar         ###   ########.fr       */
+/*   Updated: 2024/07/16 16:58:42 by seojepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "run.h"
 
-void	builtin_pwd(void)
+void	builtin_pwd(char **env)
 {
 	char	cwd[1024];
 
 	if (getcwd(cwd, sizeof(cwd)))
+	{
 		printf("%s\n", cwd);
+		free(*env);
+		*env = ckm(ft_strdup("?=0"));
+	}
 	else
+	{
 		perror("pwd failed");
+		free(*env);
+		*env = ckm(ft_strdup("?=1"));
+	}
 }
