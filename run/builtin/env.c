d5921d67d961a06f44ec0887579d6369a97fa7d5
dlstodfpwjsem <seojepar@student.42seoul.kr>
2024-07-16 15:05:19 +0900

    Docs: builtin_env exit_status 처리 추가

--- run/builtin/env.c
+++ run/builtin/env.c
@@ -6,11 +6,12 @@
 /*   By: seojepar <seojepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 21:45:48 by seojepar          #+#    #+#             */
-/*   Updated: 2024/07/12 15:12:49 by seojepar         ###   ########.fr       */
+/*   Updated: 2024/07/16 15:00:50 by seojepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "run.h"
+#include "parse.h"
 
 void	builtin_env(char **argv, char **env)
 {
@@ -19,6 +20,8 @@ void	builtin_env(char **argv, char **env)
 	if (argv[1] != NULL)
 	{
 		printf("env: %s: No such file or directory", argv[1]);
+		free(env[0]);
+		env[0] = ckm(ft_strdup("?=127"));
 		return ;
 	}
 	// 0은 ? 있어서 제외
@@ -28,4 +31,6 @@ void	builtin_env(char **argv, char **env)
 		printf("%s\n", env[i]);
 		i++;
 	}
+	free(env[0]);
+	env[0] = ckm(ft_strdup("?=0"));
 }
