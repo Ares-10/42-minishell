3e47ccc97656dd96848bbadd31285a747022567a
dlstodfpwjsem <seojepar@student.42seoul.kr>
2024-07-17 11:26:40 +0900

    Fix: exit hello; numeric arg error 수정

--- run/builtin/builtin_exit.c
+++ run/builtin/builtin_exit.c
@@ -6,27 +6,45 @@
 /*   By: seojepar <seojepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 21:46:18 by seojepar          #+#    #+#             */
-/*   Updated: 2024/07/16 16:57:37 by seojepar         ###   ########.fr       */
+/*   Updated: 2024/07/17 11:26:02 by seojepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "run.h"
 #include <stdint.h>
 
+int	check_numeric(char *str)
+{
+	if (*str == '+' || *str == '-')
+		str++;
+	if (*str == '\0')
+		return (FALSE);
+	while (*str && ft_isdigit(*str))
+		str++;
+	return (*str == '\0');
+}
+
 void	builtin_exit(char **argv, char **env)
 {
 	uint8_t	exit_code;
 
-	write_error("exit\n");
 	if (argv[1] != NULL && argv[2] != NULL)
 	{
-		write_error("minishell: exit: too many arguments");
+		write_error("minishell: exit: too many arguments\n");
 		free(*env);
 		*env = ft_strdup("?=1");
 		return ;
 	}
 	exit_code = 0;
 	if (argv[1] != NULL)
-		exit_code = (atoi(argv[1]) % 256);
+	{
+		if (check_numeric(argv[1]))
+			exit_code = (ft_atoi(argv[1]) % 256);
+		else
+		{
+			exit_code = 255;
+			write_error("minishell: exit: numeric argument required\n");
+		}
+	}
 	exit(exit_code);
 }
