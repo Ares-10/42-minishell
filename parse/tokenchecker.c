cefdddb1c093a25b2c7408debd37d96fe04abb13
Ares-10 <cholionking1@naver.com>
2024-07-19 20:21:40 +0900

    fix: 파이프라인 파싱 에러 검사 수정

--- parse/tokenchecker.c
+++ parse/tokenchecker.c
@@ -6,7 +6,7 @@
 /*   By: hyungcho <hyungcho@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 21:33:57 by hyungcho          #+#    #+#             */
-/*   Updated: 2024/07/14 23:20:21 by hyungcho         ###   ########.fr       */
+/*   Updated: 2024/07/19 20:20:27 by hyungcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@
 
 #include "parse.h"
 
-static int	puterr_int_retuen(char *msg)
+static int	puterr_int_ret(char *msg)
 {
 	puterr(msg);
 	return (FAILURE);
@@ -41,14 +41,16 @@ int	token_check(t_list *token_list)
 		token = token_list->content;
 		if (token->type == T_PIPE)
 		{
-			if (prv_token == NULL || prv_token->type != T_WORD)
-				return (puterr_int_retuen("wrong pipeline input"));
+			if (prv_token == NULL || prv_token->type != T_WORD
+				|| token_list->next == NULL
+				|| ((t_token *)token_list->next->content)->type == T_PIPE)
+				return (puterr_int_ret("wrong pipeline input"));
 		}
 		else if (token->type == T_REDIRECT)
 		{
 			if (token_list->next == NULL
 				|| ((t_token *)token_list->next->content)->type != T_WORD)
-				return (puterr_int_retuen("wrong redirect input"));
+				return (puterr_int_ret("wrong redirect input"));
 		}
 		prv_token = token_list->content;
 		token_list = token_list->next;
