8ed81ca026ed7ac04b4f08b490dedb9a6903039c
dlstodfpwjsem <seojepar@student.42seoul.kr>
2024-07-19 16:54:16 +0900

    Fix: env 1로 출력해야하는데 2로 출력한것 수정

--- run/builtin/builtin_env.c
+++ run/builtin/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: seojepar <seojepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 21:45:48 by seojepar          #+#    #+#             */
-/*   Updated: 2024/07/17 23:10:30 by seojepar         ###   ########.fr       */
+/*   Updated: 2024/07/19 16:51:59 by seojepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void	builtin_env(char **argv, char **env)
 	i = 1;
 	while (env[i] != NULL)
 	{
-		ft_putendl_fd(env[i], 2);
+		ft_putendl_fd(env[i], 1);
 		i++;
 	}
 	free(env[0]);
