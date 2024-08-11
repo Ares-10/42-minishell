73f8c8d269a5c27c28b3552ad5247984991e6185
dlstodfpwjsem <seojepar@student.42seoul.kr>
2024-08-11 20:36:55 +0900

    Refactor: builtin_pwd.c

--- run/builtin/builtin_pwd.c
+++ run/builtin/builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: seojepar <seojepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 16:46:50 by seojepar          #+#    #+#             */
-/*   Updated: 2024/08/11 20:34:05 by seojepar         ###   ########.fr       */
+/*   Updated: 2024/08/11 20:36:24 by seojepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,9 @@ void	builtin_pwd(char **env)
 	char	cwd[1024];
 
 	if (getcwd(cwd, sizeof(cwd)))
-	{
 		ft_putendl_fd(cwd, STDOUT_FILENO);
-		free(*env);
-		*env = ckm(ft_strdup("?=0"));
-	}
 	else
-	{
 		ft_putendl_fd(ft_getenv("PWD", env), STDOUT_FILENO);
-		free(*env);
-		*env = ckm(ft_strdup("?=1"));
-	}
+	free(*env);
+	*env = ckm(ft_strdup("?=0"));
 }
